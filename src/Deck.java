e1e196233ac8aefdb6335bd6178c8ce8e3137db1
isxxco <43239925+isxxco@users.noreply.github.com>
2019-04-03 19:58:55 -0400

    Create Deck.java

---
+++ src/Deck.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+
+public class Deck extends ArrayList<Card>{
+
+	//constructor inspired by AP Lab elevens
+	public Deck(String[] suits, String[] ranks, int[] values) {
+		for (int i = 0; i < ranks.length; i++) {
+			for (String suitString : suits) {
+				this.add(new Card(ranks[i], suitString, values[i]));
+			}
+		}
+		Collections.shuffle(this);
+	}
+
+	public Card deal() {
+		if(this.size() > 0) {
+			return this.remove(0);
+		}
+		return null;
+	}
+}
